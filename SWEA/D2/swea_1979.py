a9db2206db4984f3e40b4d714367b610508fb777
Jo-Myounghee <whaudg98@naver.com>
2020-08-15 00:17:59 +0900

    swea_1979

--- SWEA/D2/swea_1979.py
+++ SWEA/D2/swea_1979.py
@@ -6,12 +6,29 @@ T = int(input())
 
 for tc in range(1, T+1):
     N, K = map(int, input().split())
+    arr = []
     for _ in range(N):
-        number = list(map(str, input().split()))
-        str_num = ''
-        for i in range(N):
-            str_num+=number[i]
-        cnt = 0
-        if '1'*K in str_num:
+        number = list(map(int, input().split()))
+        arr.append(number)
+    cnt = 0
+    for i in range(N):
+        total_row = 0
+        total_column = 0
+        for j in range(N):
+            if arr[i][j] == 1:
+                total_row += 1
+            else:
+                if total_row == K:
+                    cnt += 1
+                total_row = 0
+            if arr[j][i] == 1:
+                total_column += 1
+            else:
+                if total_column == K:
+                    cnt += 1
+                total_column = 0
+        if total_row == K:
             cnt += 1
-    print(cnt)
+        if total_column == K:
+            cnt += 1
+    print(f'#{tc} {cnt}')
