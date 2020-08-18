016edc2ab53d4c24f4b844afa665f5bb753093c8
Jo-Myounghee <whaudg98@naver.com>
2020-08-18 21:40:19 +0900

    swea 1926

--- SWEA/D2/swea_1926.py
+++ SWEA/D2/swea_1926.py
@@ -3,7 +3,17 @@ import sys
 sys.stdin = open("./input_data/input_1926.txt", "r")
 
 N = int(input())
-
+lst = [3, 6, 9]
 for i in range(1, N+1):
     k = str(i)
-    
+    cnt = 0
+    for j in lst:
+        j = str(j)
+        for t in range(len(k)):
+            if j in k[t]:
+                cnt += 1
+    if cnt == 0:
+        result = i
+    else:
+        result = '-' * cnt
+    print(result, end=' ')
