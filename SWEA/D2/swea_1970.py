bc47e673c6e3b31be5548b61d453774186082cf1
Jo-Myounghee <whaudg98@naver.com>
2020-08-18 17:54:17 +0900

    swea 1970

--- SWEA/D2/swea_1970.py
+++ SWEA/D2/swea_1970.py
@@ -8,10 +8,12 @@ for tc in range(1, T+1):
     money = int(input())
     moneys = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
     numbers = [0] * 8
-    # print(numbers)
-    while money != 0:
+    print(f'#{tc}')
+    while money >= 10:
         for i in range(len(moneys)):
             numbers[i] = money // moneys[i]
             money = money % moneys[i]
-    print(numbers)
+            print(numbers[i], end=' ')
+    print()
+    
 
