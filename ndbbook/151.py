3e3f80ef0432c9bbf021a3985658f31af59c895e
Jo-Myounghee <whaudg98@naver.com>
2020-09-02 01:49:43 +0900

    modify dfs algorithm

--- ndbbook/151.py
+++ ndbbook/151.py
@@ -1,18 +1,21 @@
-n, m = map(int, input().split())
-
-graph = [list(map(int, input())) for _ in range(n)]
-
 def DFS(x, y):
     if x <= -1 or x >= m or y <= -1 or y >= n:
         return False
     if graph[y][x] == 0:
         graph[y][x] = 1
-        DFS(x-1, y)
-        DFS(x, y-1)
-        DFS(x+1, y)
-        DFS(x, y+1)
+
+        dx = [1, 0, -1, 0]
+        dy = [0, 1, 0, -1]
+
+        for dir in range(4):
+            testX = x + dx[dir]
+            testY = y + dy[dir]
+            DFS(testX, testY)
         return True
     return False
+
+n, m = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(n)]
 
 result = 0
 for y in range(n):
