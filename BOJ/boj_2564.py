3ee5afd15b04f5ab5d7a2b1519d50e4665ca32c3
Jo-Myounghee <whaudg98@naver.com>
2020-09-23 20:30:22 +0900

    boj 2564

---
+++ BOJ/boj_2564.py
@@ -0,0 +1,30 @@
+def dist(d, o):
+    if d == 1:
+        return TOT - o
+    elif d == 2:
+        return Y + o
+    elif d == 3:
+        return o
+    else:
+        return TOT - (X + o)
+
+X, Y = map(int, input().split())
+TOT = (X + Y) * 2
+N = int(input())
+shop = []
+for i in range(N + 1):
+    D, O = map(int, input().split())
+    wd = dist(D, O)
+    if i == N:
+        sp = wd
+    else:
+        shop.append(wd)
+SUM = 0
+for i in range(N):
+    wat = abs(sp - shop[i])
+    rwat = TOT - wat
+    if wat >= rwat:
+        SUM += rwat
+    else:
+        SUM += wat
+print(SUM)
