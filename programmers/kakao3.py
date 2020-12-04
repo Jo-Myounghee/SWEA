aef89aaf8981643a4d2ad77af33f3fd85b90505f
Jo-Myounghee <whaudg98@naver.com>
2020-12-05 00:13:45 +0900

    edit 3

--- programmers/kakao3.py
+++ programmers/kakao3.py
@@ -3,25 +3,32 @@ def solution(words, queries):
     for i in queries:
         cnt = 0
         for j in words:
-            no_same = False
             if len(i) != len(j):
                 continue
             else:
                 k = 0
-                # 효율성 테케3이 ?????인 글자인듯
                 if i == '?'*len(i):
                     cnt += 1
+
                 elif i[k] == '?':
-                    while i[k] == '?':
-                        k += 1
+                    temp_word = i[::-1]
+                    k = len(j) - temp_word.index('?')
                     if i[k:] == j[k:]:
                         cnt += 1
                 else:
-                    while i[k] != '?':
-                        k += 1
+                    k = i.index('?')
                     if i[:k] == j[:k]:
                         cnt += 1
         answer.append(cnt)
     return answer
 
 print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
+
+# 시간 엄청 줄인 코드
+
+# a = str(11123456)
+# a = a[::-1]
+# inx = str(a).index('1')
+# print(inx)
+# a = a[::-1]
+# print(a[len(a)-inx-1])
