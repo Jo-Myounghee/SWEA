4f837f5d22478ba0a6ab3bf63a31f5f5f095ca30
Jo-Myounghee <whaudg98@naver.com>
2021-01-14 23:13:04 +0900

    DFS-연구소 수정

--- ndbbook/DFS, BFS/16_연구소.py
+++ ndbbook/DFS, BFS/16_연구소.py
@@ -8,69 +8,68 @@
 0 1 0 0 0 0 0
 0 1 0 0 0 0 0
 '''
-from itertools import combinations
+n, m = map(int, input().split())
+data = [] # 초기 맵 리스트
+temp = [[0] * m for _ in range(n)] # 벽을 설치한 뒤의 맵 리스트
 
-# for문 이용해서 DFS사용하기, visited 배열과 함께
-def DFS(x, y):
-    visited[y][x] = True
-    dir = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+for _ in range(n):
+    data.append(list(map(int, input().split())))
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+result = 0
+
+# 바이러스가 퍼질 때
+def virus(x, y):
     for i in range(4):
-        temp_x = dir[i][0] + x
-        temp_y = dir[i][1] + y
-        if 0 <= temp_x < M and 0 <= temp_y < N:
-            if temp_board[temp_y][temp_x] != 1 and visited[temp_y][temp_x] == False:
-                temp_board[temp_y][temp_x] = 2
-                return DFS(temp_x, temp_y)
-        continue
+        nx = x + dx[i]
+        ny = y + dy[i]
+        # 상, 하, 좌, 우 중에서 바이러스가 퍼질 수 있는 경우
+        if nx >= 0 and nx < n and ny >= 0 and ny < m:
+            if temp[nx][ny] == 0:
+                # 해당 위치에 바이러스 배치하고, 다시 재귀적으로 수행
+                temp[nx][ny] = 2
+                virus(nx, ny)
 
-    return
+# 현재 맵에서 안전 영역의 크기 계산
+def get_score():
+    score = 0
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == 0:
+                score += 1
+    return score
 
-N, M = map(int, input().split(' '))
-board = [list(map(int, input().split(' '))) for _ in range(N)]
-# 1. 0인 부분 위치를 모두 한 리스트에 담기
-zero_lst = []
-wall_len = 0
 
+# DFS를 이용해 울타리를 설치하면서 매번 안전 영역의 크기 계산
+def dfs(count):
+    global result
+    # 울타리가 3개 설치된 경우
+    if count == 3:
+        for i in range(n):
+            for j in range(m):
+                temp[i][j] = data[i][j]
 
-for y in range(N):
-    for x in range(M):
-        if board[y][x] == 0:
-            zero_lst.append((x, y))
-        elif board[y][x] == 1:
-            wall_len += 1
-print(wall_len, 'wall_len')
-zero_num = len(zero_lst)
-# 2. 리스트안에서 3개 뽑기 -> combination으로 구하기
-wall_lst = list(combinations(zero_lst, 3))
-total_safe = 0 # 바이러스 영역의 최소 크기
-# 3. 각각의 경우(3개를 골랐을 때)에서 일어날 수 있는 안전영역 크기 구하기
-for i in range(len(wall_lst)):
-    visited = [[0] * M for _ in range(N)]
-    temp_wall_lst = wall_lst[i]
-    temp_safe = 0
-    temp_board = [board[k][0:M] for k in range(len(board))]
-    for j in range(3):
-        x, y = temp_wall_lst[j][0], temp_wall_lst[j][1]
-        temp_board[y][x] = 1
+        # 각 바이러스의 위치에서 전파 진행
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j] == 2:
+                    virus(i, j)
 
-    for y in range(N):
-        for x in range(M):
-            if visited[y][x] == False and board[y][x] == 2:
-                DFS(x, y)
+        # 안전 영역의 최댓값 계산
+        result = max(result, get_score())
+        return
+    # 빈 공간에 울타리 설치
+    for i in range(n):
+        for j in range(m):
+            if data[i][j] == 0:
+                data[i][j] = 1
+                count += 1
+                dfs(count)
+                data[i][j] = 0
+                count -= 1
 
-    temp_safe = 0
-    for y in range(N):
-        for x in range(M):
-            if temp_board[y][x] == 0:
-                temp_safe += 1
-        # print(visited[y])
-    if temp_wall_lst == ((1, 0), (0, 1), (3, 5)):
-        print('########')
-        print(temp_safe)
-    if temp_safe > total_safe:
-        print(temp_safe)
-        total_safe = temp_safe
+dfs(0)
+print(result)
 
-print('yes') if board == temp_board else print('no')
-print('total_safe')
-print(total_safe)
