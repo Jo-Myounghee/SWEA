83601060e245ab9b0b14165ad6292170871505bf
whaudg98 <whaudg98@naver.com>
2021-01-27 23:47:42 +0900

    y수열실패

--- BOJ/boj_20127.py
+++ BOJ/boj_20127.py
@@ -1,37 +1,100 @@
-def is_up(numbers):
-    for i in range(1, len(numbers)):
-        if numbers[i-1] > numbers[i]:
-            return False
-    return True
+# def is_up(numbers):
+#     for i in range(1, len(numbers)):
+#         if numbers[i-1] > numbers[i]:
+#             return False
+#     return True
 
-def is_down(numbers):
-    for i in range(1, len(numbers)):
-        if numbers[i-1] < numbers[i]:
-            return False
-    return True
+# def is_down(numbers):
+#     for i in range(1, len(numbers)):
+#         if numbers[i-1] < numbers[i]:
+#             return False
+#     return True
 
 N = int(input())
 numbers = list(map(int, input().split()))
 
-no_answer = True
+first_up_cut = 0
+first_down_cut = 0
+up_cut = 0
+down_cut = 0
+up_ans = False
+down_ans = False
+for i in range(1, len(numbers)):
+    if numbers[i-1] > numbers[i]:
+        up_cut += 1
+        first_up_cut = i
+    if up_cut > 1:
+        break
 
-if N == 2:
-    print(1)
-    no_answer = False
-elif N == 1:
+if up_cut == 1:
+    temp_lst = numbers[i:] + numbers[:i]
+    for i in range(1, len(temp_lst)):
+        if temp_lst[i-1] > temp_lst[i]:
+            print(-1)
+            break
+
+
+for i in range(1, len(numbers)):
+    if numbers[i-1] > numbers[i]:
+        down_cut += 1
+        first_down_cut = i
+    if down_cut > 1:
+        break
+
+if down_cut == 1:
+    temp_lst = numbers[i:] + numbers[:i]
+    for i in range(1, len(temp_lst)):
+        if temp_lst[i-1] < temp_lst[i]:
+            print(-1)
+            break
+
+if down_cut == 0 and up_cut == 0:
     print(0)
-    no_answer = False
-else:
-    for i in range(N):
-        temp_numbers = numbers[i:] + numbers[:i]
-        if is_up(temp_numbers):
-            print(i)
-            no_answer = False
-            break
-        if is_down(temp_numbers):
-            print(i)
-            no_answer = False
-            break
 
-if no_answer:
-    print(-1)
+print(first_down_cut)
+
+
+# first = numbers[0]
+# prev = numbers[0]
+# state = ''
+# cnt = 0
+# flag = '11'
+# for i in range(1, len(numbers)):
+#     if prev < numbers[i]:
+#         if state and state != 'up':
+#             cnt += 1
+#             first = numbers[i]
+#         state = 'up'
+#     elif prev > numbers[i]:
+#         if state and state != 'down':
+#             cnt += 1
+#             first = numbers[i]
+#         state = 'down'
+
+#     if cnt > 1:
+#         print(-1)
+#         break
+
+# if cnt == 0:
+#     print(0)
+# elif cnt == 1:
+#     flag = '0'
+#     lst = numbers[numbers.index(first):] + numbers[:numbers.index(first)]
+#     if first < numbers[numbers.index(first)+1]:
+#         for i in range(1, len(numbers)):
+#             if lst[i-1] > lst[i]:
+#                 print(-1)
+#                 flag = '1'
+#                 break
+#     else:
+#         now = 'down'
+#         for i in range(1, len(numbers)):
+#             if lst[i-1] < lst[i]:
+#                 print(-1)
+#                 flag = '1'
+#                 break  
+# if flag and flag == '0':
+#     print(numbers.index(first))
+    
+    
+
