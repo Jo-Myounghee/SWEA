1d539b99962003ea81e8c96004c161fd7eac613a
whaudg98 <whaudg98@naver.com>
2021-01-28 19:41:48 +0900

    boj 7562

--- BOJ/boj_7562.py
+++ BOJ/boj_7562.py
@@ -7,9 +7,20 @@
 -> 5
 '''
 # BFS
-def move(x, y):
+def BFS(x, y):
     dir = [[1, -2], [2, -1], [2, 1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [-1, -2]]
-    for i in range(8):
+    global I, end_x, end_y
+    q = [[x, y, 0]]
+    while q:
+        nx, ny, cnt = q.pop(0)
+        if nx == end_x and ny == end_y:
+            return cnt
+        for i in range(8):
+            tx = nx + dir[i][0]
+            ty = ny + dir[i][1]
+            if 0 <= tx < I and 0 <= ty < I and visited[ty][tx] == 0:
+                visited[ty][tx] = 1
+                q.append([tx, ty, cnt+1])
 
 
 T = int(input())
@@ -17,5 +28,7 @@ for _ in range(T):
     I = int(input())
     now_x, now_y = map(int, input().split())
     end_x, end_y = map(int, input().split())
+    visited = [[0] * I for _ in range(I)]
+    print(BFS(now_x, now_y))
 
     
