0ed8fcde2eee5bf4b073ac029c3d153d85ebeed0
whaudg98 <whaudg98@naver.com>
2021-02-04 02:26:57 +0900

    boj 1929 에라토스테네스의 체

---
+++ BOJ/boj_1929.py
@@ -0,0 +1,12 @@
+M, N = map(int, input().split())
+a = [False, False] + [True]*(N-1)
+primes = []
+
+for i in range(2, N+1):
+    if a[i]:
+        if M <= i <= N:
+            primes.append(i)
+        for j in range(2*i, N+1, i):
+            a[j] = False
+
+print(*primes, sep="\n")
