22e37445848cc00e6757933188aac5657ceeba71
Jo-Myounghee <jomyounghee32@gmail.com>
2021-03-07 11:50:48 +0900

    boj 1002

---
+++ BOJ/boj_1002.py
@@ -0,0 +1,18 @@
+T = int(input())
+for tc in range(T):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    dist = ((x1-x2)**2 + (y1-y2)**2)**0.5
+    if x1 == x2 and y1 == y2 and r1 == r2:
+        print(-1)
+    elif dist == 0 and r1 != r2:
+        print(0)
+    elif dist < abs(r1 - r2):
+        print(0)
+    elif dist > abs(r1+r2):
+        print(0)
+    elif dist == abs(r1 - r2):
+        print(1)
+    elif dist == abs(r1 + r2):
+        print(1)
+    elif abs(r1-r2) < dist < abs(r1 + r2):
+        print(2)
