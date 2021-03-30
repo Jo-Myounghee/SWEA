0928282fed0a65361ad391cbb8b05b7f9ac436a6
whaudg98 <whaudg98@naver.com>
2021-03-30 21:01:53 +0900

    [python] 백준 17298 오큰수

--- BOJ/boj_17298.py
+++ BOJ/boj_17298.py
@@ -1,1 +1,17 @@
-pass
+N = int(input())
+nums = list(map(int, input().split()))
+stack = []
+answer = [-1] * N
+
+stack.append(0)
+i = 1
+
+while stack and i < N:
+    while stack and nums[stack[-1]] < nums[i]:
+        answer[stack[-1]] = nums[i]
+        stack.pop()
+
+    stack.append(i)
+    i += 1
+
+print(*answer)
