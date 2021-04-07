bc09daca2d1a4e4983bd2bba200c2280d60ac7a4
whaudg98 <whaudg98@naver.com>
2021-04-07 23:40:32 +0900

    [python] boj 17090 미로 탈출하기

--- BOJ/boj_17070.py
+++ BOJ/boj_17070.py
@@ -1,46 +1,62 @@
-def ispipe(x, y, pipe):
-    global ans, N
-    if x == N-1 and y == N-1:
-        ans += 1
+from collections import deque
 
-    if pipe == 0:
-        if x+1 < N and board[y][x+1] != 1:
-            ispipe(x+1, y, 0)
-            if y+1 < N and board[y+1][x] != 1 and board[y+1][x+1] != 1:
-                ispipe(x+1, y+1, 2)
-    elif pipe == 1:
-        if y+1 < N and board[y+1][x] != 1:
-            ispipe(x, y+1, 1)
-            if x+1 < N and board[y][x+1] != 1 and board[y+1][x+1] != 1:
-                ispipe(x+1, y+1, 2)
-    elif pipe == 2:
-        if x+1 < N and board[y][x+1] != 1:
-            ispipe(x+1, y, 0)
-        if y+1 < N and board[y+1][x] != 1:
-            ispipe(x, y+1, 1)
-            if x+1 < N and board[y][x+1] != 1 and board[y+1][x+1] != 1:
-                ispipe(x+1, y+1, 2)
+def BFS(x, y, d):
+    q = deque()
+    q.append((x, y, d))
+    road = deque()
+    road.append((x, y))
+    while q:
+        x, y, d = q.popleft()
+        visited[y][x] = True
+        if isExit[y][x] == 1:
+            while road:
+                x, y = road.popleft()
+                isExit[y][x] = 1
+            return True
+        elif isExit[y][x] == -1:
+            while road:
+                x, y = road.popleft()
+                isExit[y][x] = -1
+            return False
 
-N = int(input())
-board = [list(map(int, input().split())) for _ in range(N)]
-ans = 0
+        tx = x + dir[d][0]
+        ty = y + dir[d][1]
 
-ispipe(1, 0, 0)
-print(ans)
-'''0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'''
+        if 0 <= tx < M and 0 <= ty < N:
+            if not visited[ty][tx]:
+                q.append((tx, ty, board[ty][tx]))
+                road.append((tx, ty))
+
+            else:
+                while road:
+                    x, y = road.popleft()
+                    isExit[y][x] = -1
+                return False
+        else:
+            while road:
+                x, y = road.popleft()
+                isExit[y][x] = 1
+            return True
+    while road:
+        x, y = road.popleft()
+        isExit[y][x] = -1
+    return False
+
+N, M = map(int, input().split())
+board = [list(input()) for _ in range(N)]
+isExit = [[0]*M for _ in range(N)]
+
+dir = {'U': [0, -1], 'R': [1, 0], 'D': [0, 1], 'L': [-1, 0]}
+
+cnt = 0
+for y in range(N):
+    for x in range(M):
+        visited = [[False]*M for _ in range(N)]
+        if isExit[y][x] == 1:
+            cnt += 1
+        elif isExit[y][x] == -1:
+            continue
+        elif BFS(x, y, board[y][x]):
+            cnt += 1
+
+print(cnt)
