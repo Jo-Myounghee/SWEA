321c476c3a670e536472b210167e871f7e054fd3
Jo-Myounghee <jomyounghee32@gmail.com>
2021-04-11 23:40:29 +0900

    [python] boj 1260 DFS와 BFS

---
+++ BOJ/boj_1260.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+def dfs(v):
+    global graph
+    visited_dfs[v] = True
+    dfs_ans.append(v)
+    
+    for i in graph[v]:
+        if not visited_dfs[i]:
+            dfs(i)
+
+def bfs(v):
+    global graph
+    queue = deque([v])
+    visited_bfs[v] = True
+    while queue:
+        v = queue.popleft()
+        bfs_ans.append(v)
+        for i in graph[v]:
+            if not visited_bfs[i]:
+                queue.append(i)
+                visited_bfs[i] = True
+
+
+N, M, V = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+for i in range(M):
+    s, g = map(int, input().split())
+    graph[s].append(g)
+    graph[g].append(s)
+
+for i in range(N):
+    graph[i].sort()
+
+visited_dfs = [False] * (N+1)
+visited_bfs = [False] * (N+1)
+
+dfs_ans = []
+bfs_ans = []
+
+dfs(V)
+bfs(V)
+
+print(*dfs_ans)
+print(*bfs_ans)
+
+# dfs(V)
