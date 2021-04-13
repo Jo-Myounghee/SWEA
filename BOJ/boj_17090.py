c7d9ebc690959c46a2e97a1aa89de6e95ea2d6e5
whaudg98 <whaudg98@naver.com>
2021-04-13 23:37:57 +0900

    [python] boj 17090 미로 탈출하기

--- BOJ/boj_17090.py
+++ BOJ/boj_17090.py
@@ -1,71 +1,30 @@
-# def BFS(x, y, d):
-#     q = deque()
-#     q.append((x, y, d))
-#     road = deque()
-#     road.append((x, y))
-#     while q:
-#         x, y, d = q.popleft()
-#         visited[y][x] = True
-#         if isExit[y][x] == 1:
-#             while road:
-#                 x, y = road.popleft()
-#                 isExit[y][x] = 1
-#             return True
-#         elif isExit[y][x] == -1:
-#             while road:
-#                 x, y = road.popleft()
-#                 isExit[y][x] = -1
-#             return False
-
-#         tx = x + dir[d][0]
-#         ty = y + dir[d][1]
-
-#         if 0 <= tx < M and 0 <= ty < N:
-#             # print(tx, ty, 'tx, ty')
-#             if not visited[ty][tx]:
-#                 q.append((tx, ty, board[ty][tx]))
-#                 road.append((tx, ty))
-
-#             else:
-#                 while road:
-#                     x, y = road.popleft()
-#                     isExit[y][x] = -1
-#                 return False
-#         else:
-#             while road:
-#                 x, y = road.popleft()
-#                 isExit[y][x] = 1
-#             return True
-#     while road:
-#         x, y = road.popleft()
-#         isExit[y][x] = -1
-#     return False
-
 def dfs(x, y, visited):
     dx, dy = dir[board[y][x]]
     tx = x + dx
     ty = y + dy
-    # 만약 tx, ty가 범위를 벗어나면
+    visited.append((x, y))
+    if tx < 0 or tx >= M or ty < 0 or ty >= N:
+        for i in range(len(visited)):
+            rx, ry = visited[i]
+            isExit[ry][rx] = 1
+        return True
 
-    # 만약 tx, ty가 범위를 벗어나지 않는다면
-    if isExit[ty][tx] != 0:
-        if isExit[ty][tx] == 1:
-            return True
-        else:
-            return False
     else:
+        if (tx, ty) in visited:
+            for i in range(len(visited)):
+                rx, ry = visited[i]
+                isExit[ry][rx] = 2
+            return False
+        else:
+            ans = dfs(tx, ty, visited)
+    return ans
 
-
-    
 
 N, M = map(int, input().split())
 board = [list(input()) for _ in range(N)]
 isExit = [[0]*M for _ in range(N)]
-# 1 -> true , 2 -> false
 dir = {'U': [0, -1], 'R': [1, 0], 'D': [0, 1], 'L': [-1, 0]}
 
-for i in range(len(isExit)):
-    print(isExit[i])
 cnt = 0
 for y in range(N):
     for x in range(M):
@@ -74,8 +33,8 @@ for y in range(N):
         elif isExit[y][x] == 2:
             continue
         else:
-            if dfs(x, y):
+            ans = dfs(x, y, [])
+            if ans == True:
                 cnt += 1
-#         # print('-----------------------', x, y, 'x y --------------')
 
-# print(cnt)
+print(cnt)
