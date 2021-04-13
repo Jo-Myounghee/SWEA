b0f3d557069836ac5cfa4e9b0e0697762b945e06
whaudg98 <whaudg98@naver.com>
2021-04-14 00:38:13 +0900

    [python] boj 17090 미로 탈출하기

--- BOJ/boj_17090.py
+++ BOJ/boj_17090.py
@@ -1,40 +1,36 @@
-def dfs(x, y, visited):
-    dx, dy = dir[board[y][x]]
-    tx = x + dx
-    ty = y + dy
-    visited.append((x, y))
-    if tx < 0 or tx >= M or ty < 0 or ty >= N:
-        for i in range(len(visited)):
-            rx, ry = visited[i]
-            isExit[ry][rx] = 1
+import sys
+sys.setrecursionlimit(10**9)
+
+def dfs(x, y):
+    if x < 0 or x >= M or y < 0 or y >= N:
         return True
 
+    if board[y][x] == 'true':
+        return True
+    elif board[y][x] == 'false':
+        return False
+
+    if visited[y][x]:
+        return False
     else:
-        if (tx, ty) in visited:
-            for i in range(len(visited)):
-                rx, ry = visited[i]
-                isExit[ry][rx] = 2
-            return False
-        else:
-            ans = dfs(tx, ty, visited)
-    return ans
+        visited[y][x] = True
+        dx, dy = dir[board[y][x]]
+        tx = x + dx
+        ty = y + dy
 
+        result = dfs(tx, ty)
+        board[y][x] = ('true' if result else 'false')
+        return result
 
-N, M = map(int, input().split())
-board = [list(input()) for _ in range(N)]
-isExit = [[0]*M for _ in range(N)]
+N, M = map(int, sys.stdin.readline().rstrip().split())
+board = [list(sys.stdin.readline().rstrip()) for _ in range(N)]
+visited = [[False]*M for _ in range(N)]
 dir = {'U': [0, -1], 'R': [1, 0], 'D': [0, 1], 'L': [-1, 0]}
 
 cnt = 0
 for y in range(N):
     for x in range(M):
-        if isExit[y][x] == 1:
+        if dfs(x, y):
             cnt += 1
-        elif isExit[y][x] == 2:
-            continue
-        else:
-            ans = dfs(x, y, [])
-            if ans == True:
-                cnt += 1
 
 print(cnt)
