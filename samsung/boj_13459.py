882ebb39a900151f5795c97381d415f63c9c0c70
whaudg98 <whaudg98@naver.com>
2021-04-14 20:39:22 +0900

    [python] boj 13459 구슬 탈출

--- samsung/boj_13459.py
+++ samsung/boj_13459.py
@@ -15,24 +15,33 @@ def bfs():
         for i in range(4):
             nrx, nry, rc = move(rx, ry, dx[i], dy[i], 0)
             nbx, nby, bc = move(bx, by, dx[i], dy[i], 0)
+            # 파란 공이 구멍에 빠졌을 때 -> 무시
             if board[nby][nbx] == 'O':
                 continue
+            # 빨간 공이 구멍에 빠졌을 때 -> 종료
             if board[nry][nrx] == 'O':
                 print(1)
                 return
+            # 만약 빨간 공과 파란 공의 위치가 같다면
             if nrx == nbx and nry == nby:
+                # 카운트를 비교한다(1): 파란색이 더 먼저 도착했음 -> 빨간색이 온만큼 한 번 물러선다.
                 if rc > bc:
                     nrx, nry = nrx - dx[i], nby - dy[i]
+                # 카운트를 비교한다(2): 빨간색이 더 먼저 도착했음 -> 파란색이 온만큼 한 번 물러선다.
                 else:
                     nbx, nby = nbx - dx[i], nby - dy[i]
+            # 만약 방문하지 않은 곳이라면
             if not check[nry][nrx][nby][nbx]:
+                # 방문 체크하고
                 check[nry][nrx][nby][nbx] = True
+                # 다시 BFS
                 q.append((nrx, nry, nbx, nby, d + 1))
     print(0)
 
 N, M = map(int, input().split())
 board = [list(input()) for _ in range(N)]
 check = [[[[False]*M for _ in range(N)] for _ in range(M)] for _ in range(N)]
+# 좌 하 우 상
 dx, dy = (-1, 0, 1, 0), (0, 1, 0, -1)
 q = deque()
 
@@ -44,7 +53,9 @@ for y in range(N):
         elif board[y][x] == 'B':
             _bx, _by = x, y
 
+# 초기 상태 append
 q.append((_rx, _ry, _bx, _by, 0))
+# 초기 visit 체크
 check[_ry][_rx][_by][_bx] = True
 
 bfs()
