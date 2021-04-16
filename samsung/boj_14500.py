c96504f2d06d2b8b4f28e86771f96db8de5d19c0
whaudg98 <whaudg98@naver.com>
2021-04-16 22:29:22 +0900

    [python] boj 14500 테트로미노

--- samsung/boj_14500.py
+++ samsung/boj_14500.py
@@ -5,102 +5,46 @@ def isRoad(x, y):
     return True
 
 def findSum(x, y):
-    global board, visited, ans
+    global board, ans
     max_total = 0
-    # 1-1 (오른쪽으로 4개)
-    if isRoad(x+3, y):
-        temp_total = sum(board[y][x:x+4])
-        max_total = max(temp_total, max_total)
-    # 1-2 (아래쪽으로 4개)
-    if isRoad(x, y+3):
-        temp_total = board[y][x] + board[y+1][x] + board[y+2][x] + board[y+3][x]
-        max_total = max(temp_total, max_total)
-
-    # 2-1 (오른쪽 한개, 아래쪽 한개, 오른쪽 아래 1개)
-    if isRoad(x+1, y+1):
-        temp_total = sum(board[y][x:x+2]) + sum(board[y+1][x:x+2])
-        max_total = max(temp_total, max_total)
-
-    # 3-1 (ㄴ모양)
-    if isRoad(x+1, y+2):
-        temp_total = board[y][x] + board[y+1][x] + board[y+2][x] + board[y+2][x+1]
-        max_total = max(temp_total, max_total)
-
-    # 3-2 (ㅢ 모양)
-    if isRoad(x+2, y-1):
-        temp_total = sum(board[y][x:x+3]) + board[y-1][x+2]
-        max_total = max(temp_total, max_total)
-
-    # 3-3 (ㄱ 모양)
-    if isRoad(x + 1, y + 2):
-        temp_total = sum(board[y][x:x + 2]) + board[y + 1][x + 1] + board[y + 2][x + 1]
-        max_total = max(temp_total, max_total)
-
-    # 3-4 (ㅣㅡ 모양)
-    if isRoad(x+2, y) and isRoad(x, y+1):
-        temp_total = sum(board[y][x:x+3]) + board[y+1][x]
-        max_total = max(temp_total, max_total)
-
-    # 3-1 대칭
-    if isRoad(x+1, y-2):
-        temp_total = sum(board[y][x:x+2]) + board[y-1][x+1] + board[y-2][x+1]
-        max_total = max(temp_total, max_total)
-    # 3-2 대칭
-    if isRoad(x+2, y+1):
-        temp_total = board[y][x] + sum(board[y+1][x:x+3])
-        max_total = max(temp_total, max_total)
-    # 3-3 대칭
-    if isRoad(x+1, y) and isRoad(x, y+2):
-        temp_total = sum(board[y][x:x+2]) + board[y+1][x] + board[y+2][x]
-        max_total = max(temp_total, max_total)
-    # 3-4 대칭
-    if isRoad(x+2, y+1):
-        temp_total = sum(board[y][x:x+3]) + board[y+1][x+2]
-        max_total = max(temp_total, max_total)
-
-    # 4-1 (|-|모양)
-    if isRoad(x+1, y+2):
-        temp_total = board[y][x] + board[y+1][x] + board[y+1][x+1] + board[y+2][x+1]
-        max_total = max(temp_total, max_total)
-
-    # 4-2 (_|-모양)
-    if isRoad(x+2, y-1):
-        temp_total = sum(board[y][x:x+2]) + board[y-1][x+1] + board[y-1][x+2]
-        max_total = max(temp_total, max_total)
-    # 4-1 대칭
-    if isRoad(x+1, y-2):
-        temp_total = board[y][x] + sum(board[y-1][x:x+2]) + board[y-2][x+1]
-        max_total = max(temp_total, max_total)
-    # 4-2 대칭
-    if isRoad(x+2, y+1):
-        temp_total = sum(board[y][x:x+2]) + board[y+1][x+1] + board[y+1][x+2]
-        max_total = max(temp_total, max_total)
-
-    # 5-1 (ㅜ 모양)
-    if isRoad(x+2, y) and isRoad(x+1, y+1):
-        temp_total = sum(board[y][x:x+3]) + board[y+1][x+1]
-        max_total = max(temp_total, max_total)
-
-    # 5-2 (ㅏ모양)
-    if isRoad(x+1, y+1) and isRoad(x, y+2):
-        temp_total = board[y][x] + sum(board[y+1][x:x+2]) + board[y+2][x]
-        max_total = max(temp_total, max_total)
-
-    # 5-3 (ㅗ모양)
-    if isRoad(x+2, y) and isRoad(x+1, y-1):
-        temp_total = sum(board[y][x:x+3]) + board[y-1][x+1]
-        max_total = max(temp_total, max_total)
-
-    # 5-4 (ㅓ모양)
-    if isRoad(x+1, y-1) and isRoad(x+1, y+1):
-        temp_total = sum(board[y][x:x+2]) + board[y-1][x+1] + board[y+1][x+1]
-        max_total = max(temp_total, max_total)
+    for i in range(len(blocks)):
+        temp_sum = board[y][x]
+        for (dx, dy) in blocks[i]:
+            tx, ty = x + dx, y + dy
+            if isRoad(tx, ty):
+                temp_sum += board[ty][tx]
+        max_total = max(temp_sum, max_total)
 
     if max_total > ans:
         ans = max_total
 
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
+blocks = (
+    ((1, 0), (2, 0), (3, 0)),
+    ((0, 1), (0, 2), (0, 3)),
+
+    ((0, 1), (1, 0), (1, 1)),
+
+    ((0, 1), (0, 2), (1, 2)),
+    ((1, 0), (1, -1), (1, -2)),
+    ((1, 0), (2, 0), (2, -1)),
+    ((0, 1), (1, 1), (2, 1)),
+    ((1, 0), (1, 1), (1, 2)),
+    ((1, 0), (0, 1), (0, 2)),
+    ((1, 0), (2, 0), (0, 1)),
+    ((1, 0), (2, 0), (2, 1)),
+
+    ((0, 1), (1, 1), (1, 2)),
+    ((0, 1), (1, 0), (-1, 1)),
+    ((1, 0), (1, -1), (0, 1)),
+    ((1, 0), (1, 1), (2, 1)),
+
+    ((1, 0), (2, 0), (1, 1)),
+    ((0, 1), (1, 1), (0, 2)),
+    ((1, -1), (1, 0), (2, 0)),
+    ((1, 0), (1, -1), (1, 1))
+)
 ans = 0
 
 for y in range(N):
