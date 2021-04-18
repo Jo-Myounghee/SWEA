caa817ff5e92a1e4e034e79ae268b2c992306fd3
whaudg98 <whaudg98@naver.com>
2021-04-18 23:12:32 +0900

    [python] boj 15683 감시

--- samsung/boj_15683.py
+++ samsung/boj_15683.py
@@ -3,223 +3,72 @@ def isBoard(x, y):
         return False
     return True
 
-def countBoard(b):
-    global minAns
+def watch(x, y, k):
     cnt = 0
-    for y in range(N):
-        for x in range(M):
-            if b[y][x] == 0:
-                cnt += 1
-    minAns = min(minAns, cnt)
-    return
+    x, y = x+dx[k], y+dy[k]
+    while isBoard(x, y):
+        if not visited[y][x]:
+            visited[y][x] = True
+            cnt += 1
+        x, y = x + dx[k], y + dy[k]
+    return cnt
 
-def installCctv(cnt, origin_board):
-    # global board
-    if cnt == len(cctv):
-        print('여기')
-        # 현재 board에서 사각 지대 부분 숫자 세기 고고
-        for i in range(N):
-            print(origin_board[i])
-        countBoard(origin_board)
-        return
-    cx, cy, type = cctv[cnt]
-    board = [origin_board[i][:] for i in range(N)]
+def cctv(x, y, type, k):
+    cnt = 0
     if type == 1:
-        for i in range(4):
-            if i == 0:
-                for _x in range(cx+1, M):
-                    if isBoard(_x, cy):
-                        board[cy][_x] = True
-                    else:
-                        break
-                installCctv(cnt+1, board)
-                board = [origin_board[i][:] for i in range(N)]
-            elif i == 1:
-                for _x in range(cx-1, -1, -1):
-                    if isBoard(_x, cy):
-                        board[cy][_x] = True
-                    else:
-                        break
-                installCctv(cnt + 1, board)
-                board = [origin_board[i][:] for i in range(N)]
-            elif i == 2:
-                for _y in range(cy+1, N):
-                    if isBoard(cx, _y):
-                        board[_y][cx] = True
-                    else:
-                        break
-                installCctv(cnt + 1, board)
-                board = [origin_board[i][:] for i in range(N)]
-            elif i == 3:
-                for _y in range(cy-1, -1, -1):
-                    if isBoard(cx, _y):
-                        board[_y][cx] = True
-                    else:
-                        break
-                installCctv(cnt + 1, board)
-                board = [origin_board[i][:] for i in range(N)]
+        cnt += watch(x, y, k)
     elif type == 2:
-        for i in range(2):
-            if i == 0:
-                # 가로
-                for _x in range(cx+1, M):
-                    if isBoard(_x, cy):
-                        board[cy][_x] = True
-                    else:
-                        break
-                for _x in range(cx-1, -1, -1):
-                    if isBoard(_x, cy):
-                        board[cy][_x] = True
-                    else:
-                        break
-                installCctv(cnt + 1, board)
-                board = [origin_board[i][:] for i in range(N)]
-            else:
-                # 세로
-                for _y in range(cy+1, N):
-                    if isBoard(cx, _y):
-                        board[_y][cx] = True
-                    else:
-                        break
-                for _y in range(cy-1, -1, -1):
-                    if isBoard(cx, _y):
-                        board[_y][cx] = True
-                    else:
-                        break
-                installCctv(cnt + 1, board)
-                board = [origin_board[i][:] for i in range(N)]
+        cnt += watch(x, y, k)
+        cnt += watch(x, y, (k+2)%4)
     elif type == 3:
-        # 1
-        for i in range(4):
-            if i < 2 :
-                for _x in range(cx + 1, M):
-                    if isBoard(_x, cy):
-                        board[cy][_x] = True
-                    else:
-                        break
-                if i == 0:
-                    for _y in range(cy - 1, -1, -1):
-                        if isBoard(cx, _y):
-                            board[_y][cx] = True
-                        else:
-                            break
-                elif i == 1:
-                    for _y in range(cy + 1, N):
-                        if isBoard(cx, _y):
-                            board[_y][cx] = True
-                        else:
-                            break
-            else:
-                for _x in range(cx - 1, -1, -1):
-                    if isBoard(_x, cy):
-                        board[cy][_x] = True
-                    else:
-                        break
-                if i == 2:
-                    for _y in range(cy - 1, -1, -1):
-                        if isBoard(cx, _y):
-                            board[_y][cx] = True
-                        else:
-                            break
-                elif i == 3:
-                    for _y in range(cy + 1, N):
-                        if isBoard(cx, _y):
-                            board[_y][cx] = True
-                        else:
-                            break
-            installCctv(cnt + 1, board)
-            board = [origin_board[i][:] for i in range(N)]
+        cnt += watch(x, y, k)
+        cnt += watch(x, y, (k+1)%4)
     elif type == 4:
-        for i in range(4):
-            if i < 3:
-                for _x in range(cx+1, M):
-                    if isBoard(_x, cy):
-                        board[cy][_x] = True
-                    else:
-                        break
-                for _x in range(cx-1, -1, -1):
-                    if isBoard(_x, cy):
-                        board[cy][_x] = True
-                    else:
-                        break
-                if i == 0:
-                    for _y in range(cy-1, -1, -1):
-                        if isBoard(cx, _y):
-                            board[_y][cx] = True
-                        else:
-                            break
-                    installCctv(cnt + 1, board)
-                    board = [origin_board[i][:] for i in range(N)]
-                else:
-                    for _y in range(cy+1, N):
-                        if isBoard(cx, _y):
-                            board[_y][cx] = True
-                        else:
-                            break
-                    installCctv(cnt + 1, board)
-                    board = [origin_board[i][:] for i in range(N)]
-            else:
-                for _y in range(cy - 1, -1, -1):
-                    if isBoard(cx, _y):
-                        board[_y][cx] = True
-                    else:
-                        break
-                for _y in range(cy + 1, N):
-                    if isBoard(cx, _y):
-                        board[_y][cx] = True
-                    else:
-                        break
-                if i == 2:
-                    for _x in range(cx + 1, M):
-                        if isBoard(_x, cy):
-                            board[cy][_x] = True
-                        else:
-                            break
-                    installCctv(cnt + 1, board)
-                    board = [origin_board[i][:] for i in range(N)]
-                else:
-                    for _x in range(cx - 1, -1, -1):
-                        if isBoard(_x, cy):
-                            board[cy][_x] = True
-                        else:
-                            break
-                    installCctv(cnt + 1, board)
-                    board = [origin_board[i][:] for i in range(N)]
+        cnt += watch(x, y, k)
+        cnt += watch(x, y, (k+1)%4)
+        cnt += watch(x, y, (k+2)%4)
     elif type == 5:
-        for _x in range(cx + 1, M):
-            if isBoard(_x, cy):
-                board[cy][_x] = True
-            else:
-                break
-        for _x in range(cx - 1, -1, -1):
-            if isBoard(_x, cy):
-                board[cy][_x] = True
-            else:
-                break
-        for _y in range(cy - 1, -1, -1):
-            if isBoard(cx, _y):
-                board[_y][cx] = True
-            else:
-                break
-        for _y in range(cy + 1, N):
-            if isBoard(cx, _y):
-                board[_y][cx] = True
-            else:
-                break
-        installCctv(cnt + 1, board)
-        board = [origin_board[i][:] for i in range(N)]
+        for k in range(4):
+            cnt += watch(x, y, k)
+    return cnt
+
+def solve(n, cnt):
+    global ans, visited
+    if ans == 0:
+        return
+
+    if n == len(cctv_lst):
+        ans = min(ans, cnt)
+
+    else:
+        cx, cy, ct = cctv_lst[n]
+        temp_visited = [visited[i][:] for i in range(N)]
+        for k in range(4):
+            solve(n+1, cnt-cctv(cx, cy, ct, k))
+            visited = [temp_visited[i][:] for i in range(N)]
 
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-cctv = []
-minAns = 1e9
+cctv_lst = []
+visited = [[False]*M for _ in range(N)]
+ans = N*M
+dx, dy = (-1, 0, 1, 0), (0, 1, 0, -1)
+
 for y in range(N):
     for x in range(M):
-        if board[y][x] != 0 and board[y][x] != 6:
-            cctv.append([x, y, board[y][x]])
+        if board[y][x] == 5:
+            if not visited[y][x]:
+                visited[y][x] = True
+                ans -= 1
+            ans -= cctv(x, y, 5, 0)
+        elif board[y][x] == 6:
+            visited[y][x] = True
+            ans -= 1
+        elif board[y][x] != 0 and board[y][x] != 6:
+            cctv_lst.append([x, y, board[y][x]])
+            if not visited[y][x]:
+                visited[y][x] = True
+                ans -= 1
 
-print(cctv)
-installCctv(0, board)
-print(minAns)
-# for i in range(len(cctv)):
-#     cx, cy, ct = cctv[i]
+solve(0, ans)
+print(ans)
