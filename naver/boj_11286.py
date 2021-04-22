38238033479d791baff7ef373665ccf2acc062b0
whaudg98 <whaudg98@naver.com>
2021-04-23 00:10:14 +0900

    [python] boj 11286 절댓값 힙

---
+++ naver/boj_11286.py
@@ -0,0 +1,13 @@
+import sys, heapq
+
+N = int(input())
+lst = []
+for i in range(N):
+    n = int(sys.stdin.readline())
+    if n:
+        heapq.heappush(lst, (abs(n), n))
+    else:
+        if lst:
+            print(heapq.heappop(lst)[1])
+        else:
+            print(0)
