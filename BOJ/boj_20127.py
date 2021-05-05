805aa9443bc0715e1b610db040f4c0b4809c3c35
Jo-Myounghee <jomyounghee32@gmail.com>
2021-05-05 17:28:46 +0900

    [python] boj 20127 Y-수열

--- BOJ/boj_20127.py
+++ BOJ/boj_20127.py
@@ -1,32 +1,66 @@
+'''
+10
+3 3 3 3 4 3 3 3 3 3
+-> 4
+
+6
+2 2 3 3 4 4
+-> 0
+
+4
+1 1 2 2
+-> 0
+'''
+# 증가수열
+def isBig():
+    global N, inputs
+    cnt = 0
+    small = -1
+    for i in range(1, N):
+        if inputs[i-1] > inputs[i]:
+            cnt += 1
+            small = i
+        if cnt > 1:
+            return False
+    if inputs[-1] <= inputs[0] or cnt == 0:
+        return small
+    else:
+        return False
+
+# 감소수열
+def isSmall():
+    global N, inputs
+    cnt = 0
+    big = -1
+    for i in range(1, N):
+        if inputs[i-1] < inputs[i]:
+            cnt += 1
+            big = i
+        if cnt > 1:
+            return False
+    if inputs[-1] >= inputs[0] or cnt == 0:
+        return big
+    else:
+        return False
+
 N = int(input())
 inputs = list(map(int, input().split()))
 
-if len(set(inputs)) == 1:
+if N == 1 or len(set(inputs)) == 1:
     print(0)
+    exit()
 
-# 증가수열
-prev = inputs[0]
-change = -1
-no_answer = False
-for i in range(1, len(inputs)):
-    if prev > inputs[i]:
-        if change != -1:
-            no_answer = True
-            break
-        change = i
-        if i+1 < N and inputs[i+1]:
-
-    prev = inputs[i]
-
-# 감소수열
-prev = inputs[0]
-change = -1
-no_answer = False
-for i in range(1, len(inputs)):
-    if prev < inputs[i]:
-        if change != -1:
-            no_answer = True
-            break
-        change = i
-
-    prev = inputs[i]
+big_answer = isBig()
+small_answer = isSmall()
+print(big_answer, small_answer)
+if big_answer == -1 or small_answer == -1:
+    print(0)
+else:
+    if small_answer and big_answer:
+        print(min(small_answer, big_answer))
+    elif small_answer and not big_answer:
+        print(small_answer)
+    elif big_answer and not small_answer:
+        print(big_answer)
+    else:
+        print(-1)
