fd45d19c48df7936d7ce75bfe438afd9df93b642
Jo-Myounghee <jomyounghee32@gmail.com>
2021-06-01 08:18:10 +0900

    [python] boj 15663 N과 M(8)

---
+++ BOJ/브루트 포스(N과 M)/boj_15663.py
@@ -0,0 +1,26 @@
+def func(cnt):
+    global N, M, answer, answer_lst
+    if cnt == M:
+        now = " ".join(answer)
+        if not now in answer_lst:
+            answer_lst[now] = True
+            real_answer.append(now)
+        return
+    for i in range(N):
+        if not visited[i]:
+            visited[i] = True
+            answer.append(inputs[i])
+            func(cnt+1)
+            answer.pop()
+            visited[i] = False
+
+N, M = map(int, input().split())
+inputs = list(map(str, input().split()))
+inputs.sort()
+answer = []
+answer_lst = dict()
+real_answer = []
+visited = [False] * N
+func(0)
+print(*real_answer, sep="\n")
+
