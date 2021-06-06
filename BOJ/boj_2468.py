8b0dce8c865d8b1f1c08b9310537af840cef85df
Jo-Myounghee <jomyounghee32@gmail.com>
2021-06-06 14:50:56 +0900

    [python] boj 2468 안전영역

--- BOJ/boj_2468.py
+++ BOJ/boj_2468.py
@@ -1,33 +1,28 @@
 from collections import deque
 
 
-def func(height):
+def bfs(height):
     global N, board
     visited = [[False]*N for _ in range(N)]
     cnt = 0
+    dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
     for y in range(N):
         for x in range(N):
             if not visited[y][x] and board[y][x] > height:
                 visited[y][x] = True
-                visited = bfs(x, y, visited, height)
+                land = deque()
+                land.append((x, y))
+
+                while land:
+                    nx, ny = land.popleft()
+                    for i in range(4):
+                        tx, ty = nx + dx[i], ny + dy[i]
+                        if 0 <= tx < N and 0 <= ty < N and not visited[ty][tx]:
+                            visited[ty][tx] = True
+                            if board[ty][tx] > height:
+                                land.append((tx, ty))
                 cnt += 1
     return cnt
-
-def bfs(sx, sy, visited, height):
-    global N
-    land = deque()
-    land.append((sx, sy))
-    dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
-
-    while land:
-        nx, ny = land.popleft()
-        for i in range(4):
-            tx, ty = nx + dx[i], ny + dy[i]
-            if 0 <= tx < N and 0 <= ty < N and not visited[ty][tx]:
-                visited[ty][tx] = True
-                if board[ty][tx] > height:
-                    land.append((tx, ty))
-    return visited
 
 
 N = int(input())
@@ -39,6 +34,6 @@ for i in range(N):
     max_val = max(max_val, max(board[i]))
     min_val = min(min_val, min(board[i]))
 
-for rain_height in range(max(min_val-1, 0), max_val+1):
-    answer = max(answer, func(rain_height))
+for rain_height in range(min_val-1, max_val+1):
+    answer = max(answer, bfs(rain_height))
 print(answer)
