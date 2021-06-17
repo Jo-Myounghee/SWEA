2864e70ec7f1c610e1a3d3879056e5803b2464e1
Jo-Myounghee <jomyounghee32@gmail.com>
2021-06-17 11:29:42 +0900

    [python] boj 10971 외판원 순회2

--- BOJ/브루트 포스/boj_10971.py
+++ BOJ/브루트 포스/boj_10971.py
@@ -1,21 +1,22 @@
-def dfs(start, next, value, visited):
-    global min_val
+def dfs(start, next, value, cnt):
+    global min_val, visited
 
-    if len(visited) == N:
+    if cnt == N:
         if board[next][start] != 0:
             min_val = min(min_val, value + board[next][start])
         return
 
     for i in range(N):
-        if board[next][i] != 0 and i != start and i not in visited:
-            visited.append(i)
-            dfs(start, i, value + board[next][i], visited)
-            visited.pop()
+        if board[next][i] != 0 and i != start and not visited[i]:
+            visited[i] = True
+            dfs(start, i, value + board[next][i], cnt + 1)
+            visited[i] = False
 
 
 N = int(input())
 board = [list(map(int, input().split())) for _ in range(N)]
+visited = [False] * N
 min_val = 1e9
 for i in range(N):
-    dfs(i, i, 0, [i])
+    dfs(i, i, 0, 1)
 print(min_val)
