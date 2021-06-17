0fdce9da17d2dffdb006e2c22a91d96a7e467039
Jo-Myounghee <jomyounghee32@gmail.com>
2021-06-17 22:53:47 +0900

    [python] boj 1158 요세푸스 문제

--- BOJ/boj_1158.py
+++ BOJ/boj_1158.py
@@ -1,1 +1,18 @@
-pass
+def game(s):
+    global arr
+
+    answer = []
+    if not answer:
+        now = s-1
+        a = arr.pop(now)
+        answer.append(a)
+    while arr:
+        now = (now + K - 1) % len(arr)
+        a = arr.pop(now)
+        answer.append(a)
+    return f"<{', '.join(map(str, answer))}>"
+
+
+N, K = map(int, input().split())
+arr = list(range(1, N+1))
+print(game(K))
