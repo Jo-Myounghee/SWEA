e629a88a135ddac90f2311a47df4786f070cc47f
Jo-Myounghee <jomyounghee32@gmail.com>
2021-07-03 00:02:49 +0900

    [python] boj 15664 N과 M(10)

--- BOJ/브루트 포스(N과 M)/boj_15664.py
+++ BOJ/브루트 포스(N과 M)/boj_15664.py
@@ -1,7 +1,9 @@
 def make_group(start, val):
     global answer
     if len(val) == M:
-        answer.append(' '.join(map(str, val)))
+        now = ' '.join(map(str, val))
+        if now not in answer:
+            answer.append(' '.join(map(str, val)))
         return
     for i in range(start, N):
         make_group(i+1, val + [nums[i]])
@@ -11,6 +13,5 @@ nums = list(map(int, input().split()))
 nums.sort()
 answer = []
 make_group(0, [])
-answers = sorted(list(set(answer)))
-for i in answers:
+for i in answer:
     print(i)
