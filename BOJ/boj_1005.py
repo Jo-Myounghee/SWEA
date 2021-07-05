88fbcff2b9eb6f62bd3fc9dd2e6c59333702182c
Jo-Myounghee <jomyounghee32@gmail.com>
2021-07-05 20:22:42 +0900

    [python] boj 1005 ACM Craft

--- BOJ/boj_1005.py
+++ BOJ/boj_1005.py
@@ -4,17 +4,17 @@ from collections import deque, defaultdict
 def solution():
     N, K = map(int, input().split())
     times = [0] + list(map(int, input().split()))   # 건설하는데 걸리는 시간
-    cnt_lst = [0] * (N+1)   # 부모 노드 갯수
-    dct = defaultdict(list) # key: 부모, val: 자식
+    indegree = [0] * (N+1)   # 진입차수
+    graph = defaultdict(list)
     for _ in range(K):
         x, y = map(int, input().split())
-        dct[x].append(y)
-        cnt_lst[y] += 1
+        graph[x].append(y)
+        indegree[y] += 1
     GOAL = int(input())
     # start 지점 찾기(최상단 노드)
     start = []
     for i in range(1, N+1):
-        if cnt_lst[i] == 0:
+        if indegree[i] == 0:
             start.append(i)
 
     q = deque()
@@ -28,10 +28,10 @@ def solution():
         if n == GOAL:
             break
         answer.append(n)
-        for i in dct[n]:
+        for i in graph[n]:
             answer[i] = max(answer[i], answer[n]+times[i])
-            cnt_lst[i] -= 1
-            if cnt_lst[i] == 0:
+            indegree[i] -= 1
+            if indegree[i] == 0:
                 q.append(i)
     return answer[GOAL]
 
